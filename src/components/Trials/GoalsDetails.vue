86ea15bd693539d3b007e6031a70163c77f1a710
dawdud <dawdud12@gmail.com>
2019-02-25 22:33:09 +0100

    add goals logic

--- src/components/Trials/GoalsDetails.vue
+++ src/components/Trials/GoalsDetails.vue
@@ -1,37 +1,63 @@
 <template>
   <div class="dashboard" v-if="goals.length">
-      <div class="dashboard__stats">
-        <div class="dashboard__stats__info">
-          <h3>Ogólny cel</h3>
-        </div>
-     <ul v-for="(data) in  goals">
-            <li>
-              <h2>{{data.distance}}</h2>
-            </li>
-            <li>
-              <h4>{{data.date}}</h4>
-            </li>
-            <li>
-               <div class="goal__progressbar">
-                <div class="goal__progress">
-                 
-                </div>
-              </div>
-             
-            </li>
-           
-          </ul>
-          
+    <div class="dashboard__stats">
+      <div class="dashboard__stats__info">
+        <h3>Ogólny cel</h3>
+      </div>
+      <ul v-for="(data) in  goals">
+        <li>
+          <h2>{{data.distance}}</h2>
+        </li>
+        <li>
+          <h4>{{data.date}}</h4>
+        </li>
+        <li>
+          <div class="goal__progressbar">
+            <div class="goal__progress" :style="progresStyle"></div>
           </div>
+        </li>
+        <li>
+          
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
 import { mapState } from "vuex";
 export default {
-  
+  data: {
+    
+  },
+  data() {
+    
+    return {
+      progresStyle:{},
+     
+      goal:0,
+
+    };
+
+  },
   computed: {
     ...mapState(["goals"])
-  }
+  },
+  created(){
+    this.SumGoals();
+    
+  },
+  methods: {
+     SumGoals () {
+      const goalSum = Math.ceil(this.$store.getters.sumMonth*100/this.goals[0].distance) ;
+      if(goalSum<100){
+      this.progresStyle.width= `${goalSum}%`;
+      }
+      else{
+        this.progresStyle.width= 99 + "%";
+      }
+    
+    },
+  },
 };
 </script>
 
